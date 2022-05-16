9704a7f89070658ee975ded1054a55892cde1467
Thiago <thigomowszet@gmail.com>
2022-05-16 11:12:25 -0300

    Primer Ejercicio

--- Ejercicio_clase1/main.js
+++ Ejercicio_clase1/main.js
@@ -1,6 +1,8 @@
-//Crea un script en JS que le solicite al usuario ingresar mínimo 1(un) dato.
-//Luego, con JavaScript, realiza operaciones matemáticas o de concatenación sobre las entradas teniendo en cuenta el tipo de dato.
-//Al finalizar mostrar el resultados con alert() o console.log()
+/*
+Crea un script en JS que le solicite al usuario ingresar mínimo 1(un) dato.
+Luego, con JavaScript, realiza operaciones matemáticas o de concatenación sobre las entradas teniendo en cuenta el tipo de dato.
+Al finalizar mostrar el resultados con alert() o console.log()
+*/
 
 let ingreso_usuario = prompt("Hola! Como te llamas?");
 const resultado = alert("Hola " + ingreso_usuario + "! Bienvenido");
