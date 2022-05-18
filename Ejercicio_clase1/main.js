a464eb9ffdf2a877533d4c60b1578f012d5ffdfd
Thiago <thigomowszet@gmail.com>
2022-05-18 14:38:49 -0300

    Ejercicio 1

--- Ejercicio_clase1/main.js
+++ Ejercicio_clase1/main.js
@@ -6,3 +6,13 @@ Al finalizar mostrar el resultados con alert() o console.log()
 
 let ingreso_usuario = prompt("Hola! Como te llamas?");
 const resultado = alert("Hola " + ingreso_usuario + "! Bienvenido");
+
+
+// Prueba con if
+let edadUsuario = prompt("Cuantos años tienes?")
+if (edadUsuario > 18){ 
+const resultadoEdad = alert("Eres Mayor de edad")
+}
+else {
+    const resultadoEdad = alert("Eres menor de edad")
+}
