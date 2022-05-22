847d71216a54c92f7ba8b03a90c472a83853280a
Thiago <thigomowszet@gmail.com>
2022-05-22 15:09:08 -0300

    Ejercico Obligatorio 1 - Ciclos

---
+++ Ejercicio_obligatorio_clase2/main.js
@@ -0,0 +1,7 @@
+
+
+entradaUsuario = parseInt(prompt("Ingresa la tabla de multiplicar que desee saber: "));
+
+for (var i = 1; i <= 10; i++){
+    console.log(entradaUsuario + " x " + i + " = " + entradaUsuario * i)
+}
