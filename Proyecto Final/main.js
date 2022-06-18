321904c1463fdbe6455ebaf99485c9c5b5e7a720
Thiago <thigomowszet@gmail.com>
2022-06-18 19:00:10 -0300

    Proyecto Final - Thiago Mowszet

--- Proyecto Final/main.js
+++ Proyecto Final/main.js
@@ -14,12 +14,30 @@ class Turn {
 // Creo la clase lista de turnos.
 class TurnList {
     list = [];
-    constructor() {
+    constructor(table, addbutton) {
+        this.addbutton = addbutton;
+        this.table = table;
+        let tablehead = document.createElement("thead")
+        tablehead.innerHTML = `<tr><th>Nombre</th><th>Dia</th><th>Hora</th></tr>`; 
+        table.append(tablehead);
+        this.tablebody = this.table.appendChild(document.createElement("tbody"));
+        let ref = this;
+        this.addbutton.addEventListener("click", () => {
+            let dr = prompt("Seleccione la especialidad del dr: (Ej: Traumatologo)");
+            let dayH = prompt("Seleccione el dia que desee atenderse: (Ej: 25 de Abril)");
+            let hourH = prompt("Seleccione la hora: (Ej: 22hrs)");
+            let turn = document.createElement("tr");
+            turn.innerHTML = `<td>${dr}</td><td>${dayH}</td><td>${hourH}</td>`;
+            ref.tablebody.append(turn);
+        });
     }
 
     // Solicitar un turno.
     addTurno(fullName, day, hour) {
-        this.list.push(new Turn(fullName, day, hour));
+        //this.list.push(new Turn(fullName, day, hour));
+        let turn = document.createElement("tr");
+        turn.innerHTML = `<td>${fullName}</td><td>${day}</td><td>${hour}</td>`;
+        this.tablebody.append(turn);
     }
 
     // Eliminar un turno.
@@ -63,62 +81,62 @@ class TurnList {
 }
 
 
-let turnList = new TurnList;
+let turnList = new TurnList(document.getElementById("table"), document.getElementById("addTurn"));
 
-let menu = `Ingrese una opcion:
+// let menu = `Ingrese una opcion:
 
-1. Solicitar Turno.
-2. Eliminar un Turno.
-3. Listar mis Turnos.
-4. Filtar mis turnos por dia.
-5. Salir.
-`
+// 1. Solicitar Turno.
+// 2. Eliminar un Turno.
+// 3. Listar mis Turnos.
+// 4. Filtar mis turnos por dia.
+// 5. Salir.
+// `
 
-let finished = false;
+// let finished = false;
 
-// Ciclo con while y switch para manipular el menu. 
-while (!finished) {
+// Ciclo con while y switch para manipular el menu.
+// while (!finished) {
 
-    switch (prompt(menu)) {
+    // switch (prompt(menu)) {
 
-        case "1":
-            turnList.addTurno(
-                prompt("Seleccione la especialidad del dr: (Ej: Traumatologo)"),
-                prompt("Seleccione el dia que desee atenderse: (Ej: 25 de Abril)"),
-                prompt("Seleccione la hora: (Ej: 22hrs)")
-            );
-            break;
+    //     case "1":
+    //         turnList.addTurno(
+    //             prompt("Seleccione la especialidad del dr: (Ej: Traumatologo)"),
+    //             prompt("Seleccione el dia que desee atenderse: (Ej: 25 de Abril)"),
+    //             prompt("Seleccione la hora: (Ej: 22hrs)")
+    //         );
+    //         break;
 
-        case "2":
-            turnList.removeTurn(
-                prompt("Ingrese la especialidad de su doctor: (Ej: Traumatologo)"),
-                prompt("Ingrese el dia del turno: (Ej: 25 de Abril)")
-            );
-            break;
+    //     case "2":
+    //         turnList.removeTurn(
+    //             prompt("Ingrese la especialidad de su doctor: (Ej: Traumatologo)"),
+    //             prompt("Ingrese el dia del turno: (Ej: 25 de Abril)")
+    //         );
+    //         break;
 
-        case "3":
-            turnList.alertTurn()
-            break;
+    //     case "3":
+    //         turnList.alertTurn()
+    //         break;
 
-        case "4":
-            turnList.alertTurnByDay(
-                prompt("Seleccione el dia por el cual desee filtrar su turno: (Ej: 25 de abril)")
-            );
-            break;
+    //     case "4":
+    //         turnList.alertTurnByDay(
+    //             prompt("Seleccione el dia por el cual desee filtrar su turno: (Ej: 25 de abril)")
+    //         );
+    //         break;
 
-        case "5":
-            finished = true;
-            break;
+    //     case "5":
+    //         finished = true;
+    //         break;
 
-        default:
-            alert("Ingrese una opcion valida")
-            break;
-    }
-}
+    //     default:
+    //         alert("Ingrese una opcion valida")
+    //         break;
+    // }
+// }
 
-if (finished) {
-    alert("Gracias por utilizar el sistema de turnos")
+// if (finished) {
+//     alert("Gracias por utilizar el sistema de turnos")
 
-} else {
-    alert("Usted no ha ingresado ninguna opcion")
-}
+// } else {
+//     alert("Usted no ha ingresado ninguna opcion")
+// }
